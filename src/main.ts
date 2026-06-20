4b9bc33e6623c6a7610951ba1870013231804018
Bryl Magdadaro <asapsherbs@gmail.com>
2026-06-20 17:17:56 +0800

    refactor(app): move bootstrap config into main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { register } from 'swiper/element/bundle';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+
+register();
 
 bootstrapApplication(AppComponent, {
-  ...appConfig,
-  providers: [provideZoneChangeDetection(), ...appConfig.providers],
+  providers: [provideZoneChangeDetection(), provideRouter(routes)],
 }).catch((err) => console.error(err));
